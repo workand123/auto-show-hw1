3a2aa794419cd1c43dad58c330e54a99cff0b0b7
workand123 <ayan151219@gmail.com>
2025-03-30 13:16:05 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function upDate(previewPic) {
-    document.getElementById("image").style.backgroundImage = "url(" + previewPic.src + ")";
+    console.log("Mouse over:", previewPic.alt);
+
+    document.getElementById("image").style.backgroundImage = "url('" + previewPic.src + "')";
     document.getElementById("image").innerHTML = previewPic.alt;
 }
 
 function undo() {
+    console.log("Mouse out: Resetting image");
+
     document.getElementById("image").style.backgroundImage = "url('')";
     document.getElementById("image").innerHTML = "Hover over an image below to display here.";
 }
-
