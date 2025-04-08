996bc73bf99c0f546ecb4dd2b6749f89d2a805fe
workand123 <ayan151219@gmail.com>
2025-04-08 13:30:40 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ function upDate(previewPic) {
     imageDiv.style.backgroundImage = "url('" + previewPic.src + "')";
     imageDiv.innerHTML = previewPic.alt;
 }
-
 function undo() {
     console.log("Mouse leave or blur: Resetting image");
 
@@ -14,12 +13,13 @@ function undo() {
     imageDiv.innerHTML = "Мұнда суретті көрсету үшін, төмендегі суреттің үстіне апарыңыз.";
 }
 
-// Функция: Барлық суреттерге tabindex атрибутын автоматты түрде қосу
 function addTabIndex() {
     console.log("Page loaded: Adding tabindex to images");
 
     let images = document.querySelectorAll(".gallery img");
     for (let i = 0; i < images.length; i++) {
+        // Таб индексін қосу және қолжетімділік үшін title атрибутын орнату
         images[i].setAttribute("tabindex", "0");
+        images[i].setAttribute("title", images[i].alt); // Суреттердің title атрибуты да қолжетімділікті жақсартуға көмектеседі
     }
 }
